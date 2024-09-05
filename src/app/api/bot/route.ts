d6103b9c78a090a22c5b6c00a6c84419a835843f
Satya Sandeep <satyasandeep786@gmail.com>
2024-09-06 01:46:59 +0530

    feat: Add bot code to listen to commands

---
+++ src/app/api/bot/route.ts
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}`;
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+
+  const { message } = body;
+
+  try {
+    if (message?.text === "/start") {
+      const chatId = message.chat.id;
+
+      const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: `Starting the app`,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error(`Failed to send message: ${response.statusText}`);
+      }
+
+      return NextResponse.json({ status: "ok" });
+    }
+    return NextResponse.json({ status: "unhandled message" });
+  } catch (error: any) {
+    console.error("Error handling the request:", error.message);
+    return NextResponse.json({ status: "error", message: error.message });
+  }
+}
