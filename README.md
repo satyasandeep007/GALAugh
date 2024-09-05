d9e416e6df6ddab4256199c3575d7aca1ad172ff
Satya Sandeep <satyasandeep786@gmail.com>
2024-09-06 01:36:42 +0530

    feat: Init Nextjs boilerplate

---
+++ README.md
@@ -0,0 +1,9 @@
+# ETH Global 2024 Frontend
+
+This is the frontend for the ETH Global 2024 project.
+
+## Getting Started
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="py-auto mx-auto">
+      <h1 className="text-4xl font-bold text-gray-800">Home</h1>
+    </div>
+  );
+};
+
+export default HomePage;
