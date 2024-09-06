41f3128bbdc664cae4bc0789c9168eba259c2f48
Satya Sandeep <satyasandeep786@gmail.com>
2024-09-07 01:40:57 +0530

    refactor

--- src/app/api/bot/route.ts
+++ src/app/api/bot/route.ts
@@ -4,51 +4,37 @@ const TELEGRAM_API_URL = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT
 
 export async function POST(req: NextRequest) {
   const body = await req.json();
-
   const { message } = body;
 
   try {
+    const chatId = message?.chat?.id; // Extract chatId once
+
     if (message?.text === "/start") {
-      const chatId = message.chat.id;
-
-      const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: `Starting the app`,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Failed to send message: ${response.statusText}`);
-      }
-
-      return NextResponse.json({ status: "ok" });
+      await sendMessage(chatId, `Starting the app`);
     } else if (message?.text) {
-      const chatId = message.chat.id;
-
-      const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          chat_id: chatId,
-          text: `Message Received: ${message.text}`,
-        }),
-      });
-
-      if (!response.ok) {
-        throw new Error(`Failed to send message: ${response.statusText}`);
-      }
-
-      return NextResponse.json({ status: "ok" });
+      await sendMessage(chatId, `Message Received: ${message.text}`);
     }
+
+    return NextResponse.json({ status: "ok" });
   } catch (error: any) {
     console.error("Error handling the request:", error.message);
     return NextResponse.json({ status: "error", message: error.message });
+  }
+}
+
+async function sendMessage(chatId: string, text: string) {
+  const response = await fetch(`${TELEGRAM_API_URL}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      chat_id: chatId,
+      text: text,
+    }),
+  });
+
+  if (!response.ok) {
+    throw new Error(`Failed to send message: ${response.statusText}`);
   }
 }
